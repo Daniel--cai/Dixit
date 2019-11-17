077b84a54c1ff534f61f3be2e552cdbef370b46e
Daniel <daniel.cai92@gmail.com>
2019-11-17 23:15:55 +1100

    Add theme ui for styled components

--- Dixit.Client/src/components/App.tsx
+++ Dixit.Client/src/components/App.tsx
@@ -5,15 +5,19 @@ import { Lobby, Game } from "../routes";
 import { ConnectedRouter } from "connected-react-router";
 import { Route } from "react-router";
 import { BreakpointProvider } from "react-socks";
+import theme from "../theme"
+import {ThemeProvider} from "theme-ui";
 const App: React.FC<any> = ({ history }) => {
   return (
     <ConnectedRouter history={history}>
       <BreakpointProvider>
+      <ThemeProvider theme={theme}>
         <div className="app">
           <Route exact path="/" component={Lobby} />
           <DefaultLayout exact path="/lobby/:code" component={Lobby} />
           <DefaultLayout exact path="/game/:code" component={Game} />
         </div>
+        </ThemeProvider>
       </BreakpointProvider>
     </ConnectedRouter>
   );
---
+++ Dixit.Client/src/theme/index.ts
@@ -0,0 +1,17 @@
+export default {
+    fonts: {
+      body: 'system-ui, sans-serif',
+      heading: '"Avenir Next", sans-serif',
+      monospace: 'Menlo, monospace',
+    },
+    colors: {
+      text: '#000',
+      background: '#fff',
+      primary: '#ffffff',
+      nav: '#ffffff'
+    },
+    button: {
+      text:'#0fa6c5',
+      background: '#9283d6'
+    }
+  }
