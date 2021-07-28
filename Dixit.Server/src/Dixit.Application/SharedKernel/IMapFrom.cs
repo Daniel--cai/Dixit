408ffe3bb61eec69c0dd6f7f79bb3e34d7926ee1
Daniel <daniel.cai92@gmail.com>
2021-07-29 00:48:48 +1000

    Move back to servcer

---
+++ Dixit.Server/src/Dixit.Application/SharedKernel/IMapFrom.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Dixit.Application.SharedKernel
+{
+    public interface IMapFrom<T>
+    {
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
