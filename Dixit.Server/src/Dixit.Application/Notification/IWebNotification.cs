f94997c62b8499674a2ad3101f76dca56f628249
Daniel <daniel.cai92@gmail.com>
2021-07-29 00:40:57 +1000

    Move events to application

---
+++ Dixit.Server/src/Dixit.Application/Notification/IWebNotification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dixit.Application.Notification
+{
+    public interface IWebNotification<T>
+    {
+        Task SendAsync(T message, CancellationToken cancellationToken = default);
+    }
+}
